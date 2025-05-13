152f2955ba311f790504ccdac21b78e05be86f31
hugschweize <hugschweize@PAPP-N121-M910>
2025-05-13 16:04:55 +0200

    Ajoutez des fichiers projet.

---
+++ Auto Clicker Special/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Auto_Clicker_Special
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.autoCliker = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-98, 360);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1033, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "_________________________________________________________________________________" +
+    "________________________________________________________________________________" +
+    "__________";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(410, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(9, 247);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "l\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\nl\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-127, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1033, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "_________________________________________________________________________________" +
+    "________________________________________________________________________________" +
+    "__________";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(274, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(283, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Auto Clicker SPECIAL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(96, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 29);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Type of Button";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(525, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(136, 29);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Start Button";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(410, 248);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(1033, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "_________________________________________________________________________________" +
+    "________________________________________________________________________________" +
+    "__________";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(489, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 75);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Training";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.SkyBlue;
+            this.label7.Location = new System.Drawing.Point(569, 167);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 22);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "F6";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(560, 201);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 22);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Enter";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.SkyBlue;
+            this.label10.Location = new System.Drawing.Point(139, 201);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(84, 22);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Left Click";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(139, 272);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(96, 22);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "Right Click";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(287, 400);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(250, 13);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Is it the same auto clicker than \"Op Auto Clicker \" ?";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(327, 417);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "YES";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(413, 417);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "No, I swear on my life";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // autoCliker
+            // 
+            this.autoCliker.Enabled = true;
+            this.autoCliker.Interval = 1;
+            this.autoCliker.Tick += new System.EventHandler(this.autoCliker_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Key_Down);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer autoCliker;
+    }
+}
+
---
+++ Auto Clicker Special/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Auto_Clicker_Special
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, int dx, int dy, uint cButtons, uint dwExtraInfo);
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const uint MOUSEEVENTF_LEFTUP = 0x04;
+        private const uint MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const uint MOUSEEVENTF_RIGHTUP = 0x10;
+        bool Cliker = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("SHUT UP, THIS IS THE BEST AUTO CLICKER EVER CREATED !!!!");
+            MessageBox.Show("YOU ARE THE DUMBEST KID I'VE SEEN IN MY ENTIRE LIFE !");
+            MessageBox.Show("you are BAD :(");
+            MessageBox.Show("you have a ridicul life");
+            MessageBox.Show("you are cringe !");
+            MessageBox.Show("good bye, have a bad day :(:(:(");
+            this.Hide();
+            await Task.Delay(3000);
+            MessageBox.Show(":(");
+            MessageBox.Show("bad guy !");
+            MessageBox.Show("go play \"adopt me\" little gay kid");
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+            button2.Visible = false;
+            label12.Visible = false;
+            MessageBox.Show("You are a good guy :)");
+            MessageBox.Show("Your life is incredible ^^");
+            MessageBox.Show("have a nice day 👍");
+            MessageBox.Show(":3");
+            MessageBox.Show("💓");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ( button1.BackColor == Color.White)
+            {
+                button1.BackColor = Color.Blue;
+                return;
+            }
+            else
+            {
+                button1.BackColor = Color.White;
+                return;
+            }
+        }
+
+        private void autoCliker_Tick(object sender, EventArgs e)
+        {
+            if (Cliker == true)
+            {
+                DoMouseClick();
+            }
+            
+        }
+        public void DoMouseClick()
+        {
+                //simule un clic a la position courante du curseur
+                int X = Cursor.Position.X;
+                int Y = Cursor.Position.Y;
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
+        private void Key_Down(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F6 && Cliker == false) 
+            {
+                Cliker = true;
+                MessageBox.Show("overt");
+                return;
+            }
+            if (e.KeyCode == Keys.F6 && Cliker == true) 
+            {
+                Cliker = false;
+                MessageBox.Show("fwew");
+                return;
+            }
+        }
+    }
+}
