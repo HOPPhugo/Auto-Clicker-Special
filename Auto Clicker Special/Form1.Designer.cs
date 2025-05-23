14e10336917470b8eb8370920c914caf2a594a3b
Hugo Schweizer <hugschweize@etmlnet.local>
2025-05-23 11:13:48 +0200

    mise à jour : possibilité de changer la vitesse de cliques par ms

--- Auto Clicker Special/Form1.Designer.cs
+++ Auto Clicker Special/Form1.Designer.cs
@@ -47,6 +47,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -218,11 +222,54 @@
             this.label5.Text = "How to calcul the Cps?";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 377);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 23;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 380);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Rapidité du clique :";
+            // 
+            // button5
+            // 
+            this.button5.ForeColor = System.Drawing.Color.Blue;
+            this.button5.Location = new System.Drawing.Point(244, 378);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(14, 19);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "?";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(152, 381);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 13);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "Maximum 20 !";
+            this.label9.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label2);
@@ -265,6 +312,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- Auto Clicker Special/Form1.cs
+++ Auto Clicker Special/Form1.cs
@@ -6,11 +6,12 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Auto_Clicker_Special
 {
@@ -51,9 +52,11 @@ namespace Auto_Clicker_Special
         bool Cliker = false;
         int clicks = 0;
         int chrono = 0;
+        int Sringtoint;
         bool train = false;
+        bool chaine;
         bool boocle = false;
-
+        int CliqueSpeed;
         public Form1()
         {
             InitializeComponent();
@@ -156,20 +159,117 @@ namespace Auto_Clicker_Special
         }
 
         // Tick du timer de l'auto clicker, fait 20 clics rapides
-        private void autoCliker_Tick(object sender, EventArgs e)
+        private async void autoCliker_Tick(object sender, EventArgs e)
         {
+            if (textBox1.Text.Length == 0)
+            {
+                CliqueSpeed = 20;
+            }
             if (Cliker == true)
             {
-                DoMouseClick(); DoMouseClick1(); DoMouseClick2(); DoMouseClick3(); DoMouseClick4();
-                DoMouseClick5(); DoMouseClick6(); DoMouseClick7(); DoMouseClick8(); DoMouseClick9();
-                DoMouseClick10(); DoMouseClick11(); DoMouseClick12(); DoMouseClick13(); DoMouseClick14();
-                DoMouseClick15(); DoMouseClick16(); DoMouseClick17(); DoMouseClick18(); DoMouseClick19();
+                switch (CliqueSpeed)
+                {
+                    case 1:
+                        DoMultiplesMouseClick(2);
+                        break;
+                    case 2:
+                        DoMultiplesMouseClick(4);
+                        break;
+                    case 3:
+                        DoMultiplesMouseClick(6);
+                        break;
+                    case 4:
+                        DoMultiplesMouseClick(8);
+                        break;
+                    case 5:
+                        DoMultiplesMouseClick(10);
+                        break;
+                    case 6:
+                        DoMultiplesMouseClick(12);
+                        break;
+                    case 7:
+                        DoMultiplesMouseClick(14);
+                        break;
+                    case 8:
+                        DoMultiplesMouseClick(16);
+                        break;
+                    case 9:
+                        DoMultiplesMouseClick(18);
+                        break;
+                    case 10:
+                        DoMultiplesMouseClick(20);
+                        break;
+                    case 11:
+                        DoMultiplesMouseClick(22);
+                        break;
+                    case 12:
+                        DoMultiplesMouseClick(24);
+                        break;
+                    case 13:
+                        DoMultiplesMouseClick(26);
+                        break;
+                    case 14:
+                        DoMultiplesMouseClick(28);
+                        break;
+                    case 15:
+                        DoMultiplesMouseClick(30);
+                        break;
+                    case 16:
+                        DoMultiplesMouseClick(32);
+                        break;
+                    case 17:
+                        DoMultiplesMouseClick(34);
+                        break;
+                    case 18:
+                        DoMultiplesMouseClick(36);
+                        break;
+                    case 19:
+                        DoMultiplesMouseClick(38);
+                        break;
+                    case 20:
+                        DoMultiplesMouseClick(40);
+                        break;
+                    default:
+                        break;
+                }
+                /*
+                DoMouseClick(); 
+                DoMouseClick1(); 
+                DoMouseClick2(); 
+                DoMouseClick3(); 
+                DoMouseClick4();
+                DoMouseClick5(); 
+                DoMouseClick6(); 
+                DoMouseClick7(); 
+                DoMouseClick8(); 
+                DoMouseClick9();
+                DoMouseClick10(); 
+                DoMouseClick11(); 
+                DoMouseClick12(); 
+                DoMouseClick13(); 
+                DoMouseClick14();
+                DoMouseClick15(); 
+                DoMouseClick16(); 
+                DoMouseClick17(); 
+                DoMouseClick18(); 
+                DoMouseClick19();
                 DoMouseClick20();
+                */
             }
         }
 
         // Fonctions de clics répétées (simulent des clics à la position du curseur)
         public void DoMouseClick() { int X = Cursor.Position.X; int Y = Cursor.Position.Y; mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0); }
+
+        public void DoMultiplesMouseClick(int clicks) {
+            for (int i = 0; i < clicks; i++)
+            {
+                DoMouseClick();
+                DoMouseClick();
+            }
+        }
+
+        /*
         public void DoMouseClick1() { DoMouseClick(); DoMouseClick(); }
         public void DoMouseClick2() { DoMouseClick(); DoMouseClick(); }
         public void DoMouseClick3() { DoMouseClick(); DoMouseClick(); }
@@ -190,6 +290,7 @@ namespace Auto_Clicker_Special
         public void DoMouseClick18() { DoMouseClick(); DoMouseClick(); }
         public void DoMouseClick19() { DoMouseClick(); DoMouseClick(); }
         public void DoMouseClick20() { DoMouseClick(); DoMouseClick(); }
+        */
 
         // Ne fait rien (hook vide pour événement clavier)
         private void Key_Down(object sender, KeyEventArgs e) { }
@@ -269,6 +370,34 @@ namespace Auto_Clicker_Special
         private void label5_Click(object sender, EventArgs e)
         {
             MessageBox.Show("How to calcul the cps?\n\nClick per secondes (CPS) = Total number of clicks / Total time in secondes\nIf you clicked 30time in 5 secondes. \nThat mean you clicked 6time per seondes.");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Entre a number between 1 and 20.\nMore the number is big, more the click speed is fast.\nIf you entre nothing, the max speed will be used");
+        }
+
+        private async void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = Regex.Replace(textBox1.Text, "[^0-9]", "");
+            textBox1.Text = Regex.Replace(textBox1.Text, @"\s", "");
+            if (textBox1.Text != ""){
+
+                Sringtoint = Convert.ToInt32(textBox1.Text);
+                if (Sringtoint != 0 && Sringtoint <= 20)
+                {
+                    chaine = false;
+                    CliqueSpeed = Sringtoint;
+                }
+                else if (Sringtoint > 20)
+                {
+                    label9.Visible = true;
+                    textBox1.Text = "";
+                    await Task.Delay(1000);
+                    label9.Visible = false;
+
+                }
+            }
         }
     }
 }
